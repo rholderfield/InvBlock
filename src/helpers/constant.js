4b4a455ae9c40f8b8a6c651f8fec900c8f17c634
rholderfield <raholderfield@gmail.com>
2022-02-18 16:30:00 -0800

    adding constants and test contract abis

---
+++ src/helpers/constant.js
@@ -0,0 +1,31 @@
+import abiProductFactory from "./ProductFactory.json";
+import abiSupplierFactory from "./SupplierFactory.json";
+import abiPurchaseOrderHeaderFactory from "./PurchaseOrderHeaderFactory.json";
+import abiSalesOrderHeaderFactory from "./SalesOrderHeaderFactory.json";
+
+
+const ProductFactory = {
+    contractABI: abiProductFactory,
+    contractAddress: '0x131A797C069A241CE1ffb67325b0c8aD585ef9a8'
+}
+
+const SupplierFactory = {
+    contractABI: abiSupplierFactory,
+    contractAddress: '0xd1C89a055be30E2587D6e7aDe07D88A8B82aD198'
+}
+
+const PurchaseOrderHeaderFactory = {
+    contractABI: abiPurchaseOrderHeaderFactory,
+    contractAddress: '0xd8Fa416DedD91e2AEa215665aEbdEf3243D65F8C'
+}
+
+const SalesOrderHeaderFactory = {
+    contractABI: abiSalesOrderHeaderFactory,
+    contractAddress: '0x44EEDc74585d31DAc62B95f76C0903fb4Ce2AB33'
+}
+
+const contractInvBlock = {
+    ProductFactory, SupplierFactory, PurchaseOrderHeaderFactory, SalesOrderHeaderFactory
+}
+
+export default contractInvBlock ;
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import Moralis from './plugins/moralis'
+import contractInvBlock from './helpers/constant'
 import router from './router'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css'
 
 createApp(App).use(router)
     .provide('$moralis', Moralis)
+    .provide('contractInvBlock', contractInvBlock)
     .use(store)
     .use(Antd)
     .mount('#app')
