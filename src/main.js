9e2753263ae8f34b4191e1dbde41bd19788c0238
rholderfield <raholderfield@gmail.com>
2022-02-26 19:29:33 -0800

    building forms for lines

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import Moralis from './plugins/moralis'
-import contractInvBlock from './helpers/constant'
-import router from './router'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import Moralis from "./plugins/moralis";
+import contractInvBlock from "./helpers/constant";
+import router from "./router";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 
-createApp(App).use(router)
-    .provide('$moralis', Moralis)
-    .provide('contractInvBlock', contractInvBlock)
-    .use(store)
-    .use(Antd)
-    .mount('#app')
+createApp(App)
+  .use(router)
+  .provide("$moralis", Moralis)
+  .provide("contractInvBlock", contractInvBlock)
+  .use(store)
+  .use(Antd)
+  .mount("#app");
--- src/views/SalesOrderAdd.vue
+++ src/views/SalesOrderAdd.vue
@@ -5,10 +5,86 @@
       padding: '8px 8px',
     }"
   >
-    <a-card title="Sales Order Details" :headStyle="{backgroundColor: '#fafafa'}">
+    <a-card
+      title="Sales Order Details"
+      :headStyle="{ backgroundColor: '#fafafa' }"
+    >
       <a-card> Order Details </a-card>
-      <a-card :style="{ marginTop: '16px' }"> Line Details </a-card>
-      <a-card actions :style="{ marginTop: '16px', backgroundColor: '#fafafa' }">
+
+      <a-card :style="{ marginTop: '16px' }">
+        <a-form
+          ref="formRef"
+          :model="dynamicValidateForm"
+          v-bind="formItemLayoutWithOutLabel"
+        >
+          <a-form-item
+            v-for="(domain, index) in dynamicValidateForm.domains"
+            :key="domain.key"
+            v-bind="index === 0 ? formItemLayout : {}"
+            :label="index === 0 ? 'Line Details' : ''"
+            :name="['domains', index, 'value']"
+            :rules="{
+              required: true,
+              message: 'can not be null',
+              trigger: 'change',
+            }"
+            >
+            <div class="listItems">
+            <div>
+              <a-typography-text>Line Amount </a-typography-text>
+              <a-input-number
+                v-model:value="domain.Amount"
+                placeholder="0.00"
+                style="width: 60%; margin-right: 8px"
+              />
+            </div>
+            <div>
+              <a-typography-text>Product Id </a-typography-text>
+              <a-input-number
+                v-model:value="domain.ProductId"
+                placeholder="000"
+                style="width: 60%; margin-right: 8px"
+              />
+            </div>
+            <div>
+              <a-typography-text>Quantity </a-typography-text>
+              <a-input-number
+                v-model:value="domain.Quantity"
+                placeholder="00"
+                style="width: 60%; margin-right: 8px"
+              />
+            </div>
+            <MinusCircleOutlined
+              v-if="dynamicValidateForm.domains.length > 1"
+              class="dynamic-delete-button"
+              :disabled="dynamicValidateForm.domains.length === 1"
+              @click="removeDomain(domain)"
+            />
+            
+            </div>
+            </a-form-item>
+
+          <a-form-item v-bind="formItemLayoutWithOutLabel">
+            <a-button type="dashed" style="width: 10%" @click="addDomain">
+              <PlusOutlined />
+              Add Line
+            </a-button>
+          </a-form-item>
+          <a-form-item v-bind="formItemLayoutWithOutLabel">
+            <a-button type="primary" html-type="submit" @click="submitForm"
+              >Submit</a-button
+            >
+            <a-button style="margin-left: 10px" @click="resetForm"
+              >Reset</a-button
+            >
+          </a-form-item>
+          
+        </a-form>
+      </a-card>
+      <a-card
+        actions
+        :style="{ marginTop: '16px', backgroundColor: '#fafafa' }"
+      >
         <a-button
           :style="{ float: 'right' }"
           type="primary"
@@ -27,26 +103,123 @@
 <style scoped>
 button.ant-btn {
   margin: 0 8px;
-  color: #05182A;
-
+  color: #05182a;
+}
+form.ant-form.ant-form-horizontal {
+ text-align: left;
 }
 
-form.ant-form.ant-form-horizontal {
-  text-align: left;
+.dynamic-delete-button {
+  cursor: pointer;
+  position: relative;
+  top: 4px;
+  font-size: 24px;
+  color: #999;
+  transition: all 0.3s;
+}
+.dynamic-delete-button:hover {
+  color: #777;
+}
+.dynamic-delete-button[disabled] {
+  cursor: not-allowed;
+  opacity: 0.5;
+}
+
+.listItems {
+  display:inline-flex;
 }
 </style>
 
 <script>
-
-export default {
-  name: "SalesOrderAdd",
-  components: {},
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons-vue";
+import { defineComponent, reactive, ref } from "vue";
+export default defineComponent({
   setup() {
-    const print = () => { console.log("Clicked")}
+    const formRef = ref();
+    const formItemLayout = {
+      labelCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 4,
+        },
+      },
+      wrapperCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 20,
+        },
+      },
+    };
+    const formItemLayoutWithOutLabel = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 20,
+          offset: 4,
+        },
+      },
+    };
+    const dynamicValidateForm = reactive({
+      domains: [],
+    });
+
+    dynamicValidateForm.domains.push({
+      value: "",
+      key: Date.now(),
+    });
+
+    const submitForm = () => {
+      formRef.value
+        .validate()
+        .then(() => {
+          console.log("values", dynamicValidateForm.domains);
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+    };
+
+    const resetForm = () => {
+      formRef.value.resetFields();
+    };
+
+    const removeDomain = (item) => {
+      let index = dynamicValidateForm.domains.indexOf(item);
+
+      if (index !== -1) {
+        dynamicValidateForm.domains.splice(index, 1);
+      }
+    };
+
+    const addDomain = () => {
+      dynamicValidateForm.domains.push({
+        value: "",
+        key: Date.now(),
+      });
+    };
 
     return {
-      print,
+      formRef,
+      formItemLayout,
+      formItemLayoutWithOutLabel,
+      dynamicValidateForm,
+      submitForm,
+      resetForm,
+      removeDomain,
+      addDomain,
     };
   },
-};
+
+  components: {
+    MinusCircleOutlined,
+    PlusOutlined,
+  },
+});
 </script>
